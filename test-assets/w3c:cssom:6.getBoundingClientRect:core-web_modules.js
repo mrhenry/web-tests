5aaaf09a95e41d01e1d26c1e14176ddf9a1425f9
romainmenke <romainmenke@gmail.com>
2021-01-23 10:36:36 +0100

    getClientBoundingRect

---
+++ test-assets/w3c:cssom:6.getBoundingClientRect:core-web_modules.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+(function (cb) {
+  var el = document.getElementById("the-fixture--positioned");
+  var clientBoundingRect = el.getBoundingClientRect();
+  cb("top" in clientBoundingRect && "left" in clientBoundingRect && "x" in clientBoundingRect && "y" in clientBoundingRect && "width" in clientBoundingRect && "height" in clientBoundingRect && clientBoundingRect.top > 0 && clientBoundingRect.left > 0 && clientBoundingRect.x > 0 && clientBoundingRect.y > 0 && clientBoundingRect.width > 0 && clientBoundingRect.height > 0);
+})(callback);
+/******/ })()
+;
