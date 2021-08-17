cf69c1b627e5e550f423a5ffdf3e6f460d543b8c
romainmenke <romainmenke@gmail.com>
2021-08-17 19:52:01 +0200

    fix

--- specifications/tc39/ecma402/11.4.6.Intl.DateTimeFormat.prototype.formatRangeToParts/test.esbuild.js
+++ specifications/tc39/ecma402/11.4.6.Intl.DateTimeFormat.prototype.formatRangeToParts/test.esbuild.js
@@ -5,6 +5,6 @@
       hourCycle: "h11",
       hour: "numeric"
     }).formatRangeToParts(1, 43201e3);
-    cb(range[0].type === "hour" && range[0].source === "startRange" && range[0].value === "1" && range[1].type === "literal" && range[1].source === "startRange" && range[1].value === " " && range[2].type === "dayPeriod" && range[2].source === "startRange" && range[2].value === "AM" && range[3].type === "literal" && range[3].source === "shared" && range[4].type === "hour" && range[4].source === "endRange" && range[4].value === "1" && range[5].type === "literal" && range[5].source === "endRange" && range[5].value === " " && range[6].type === "dayPeriod" && range[6].source === "endRange" && range[6].value === "PM");
+    cb(range[0].type === "hour" && range[0].source === "startRange" && range[0].value && range[1].type === "literal" && range[1].source === "startRange" && range[1].value === " " && range[2].type === "dayPeriod" && range[2].source === "startRange" && range[2].value && range[3].type === "literal" && range[3].source === "shared" && range[4].type === "hour" && range[4].source === "endRange" && range[4].value && range[5].type === "literal" && range[5].source === "endRange" && range[5].value === " " && range[6].type === "dayPeriod" && range[6].source === "endRange" && range[6].value);
   })(callback);
 })();
