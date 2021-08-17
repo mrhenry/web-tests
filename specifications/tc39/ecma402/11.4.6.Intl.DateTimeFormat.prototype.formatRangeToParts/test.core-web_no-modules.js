dad36e3c4e952df959f9bdb58717d1c5ea0a3ef8
romainmenke <romainmenke@gmail.com>
2021-08-17 22:31:11 +0200

    fix

--- specifications/tc39/ecma402/11.4.6.Intl.DateTimeFormat.prototype.formatRangeToParts/test.core-web_no-modules.js
+++ specifications/tc39/ecma402/11.4.6.Intl.DateTimeFormat.prototype.formatRangeToParts/test.core-web_no-modules.js
@@ -26280,11 +26280,12 @@ function Intl_DateTimeFormat_locale_en_typeof(obj) { "@babel/helpers - typeof"; 
 
 
 (function (cb) {
-  var parts = new Intl.DateTimeFormat('en', {
+  var range = new Intl.DateTimeFormat('en', {
     hourCycle: 'h11',
-    hour: 'numeric'
-  }).formatRangeToParts(43201000, 43201000 * 2);
-  cb(!!parts && !!parts.length);
+    hour: 'numeric',
+    timeZone: 'UTC'
+  }).formatRangeToParts(43201000 * 4, 43201000 * 5);
+  cb(range[0].type === 'hour' && range[0].source === 'startRange' && range[0].value === '0' && range[1].type === 'literal' && range[1].source === 'startRange' && range[1].value === ' ' && range[2].type === 'dayPeriod' && range[2].source === 'startRange' && range[2].value === 'AM' && range[3].type === 'literal' && range[3].source === 'shared' && range[4].type === 'hour' && range[4].source === 'endRange' && range[4].value === '0' && range[5].type === 'literal' && range[5].source === 'endRange' && range[5].value === ' ' && range[6].type === 'dayPeriod' && range[6].source === 'endRange' && range[6].value === 'PM');
 })(callback);
 }();
 /******/ })()
--- specifications/tc39/ecma402/11.4.6.Intl.DateTimeFormat.prototype.formatRangeToParts/test.esbuild.js
+++ specifications/tc39/ecma402/11.4.6.Intl.DateTimeFormat.prototype.formatRangeToParts/test.esbuild.js
@@ -1,10 +1,11 @@
 (function() {
   // specifications/tc39/ecma402/11.4.6.Intl.DateTimeFormat.prototype.formatRangeToParts/test.pure.js
   (function(cb) {
-    var parts = new Intl.DateTimeFormat("en", {
+    var range = new Intl.DateTimeFormat("en", {
       hourCycle: "h11",
-      hour: "numeric"
-    }).formatRangeToParts(43201e3, 43201e3 * 2);
-    cb(!!parts && !!parts.length);
+      hour: "numeric",
+      timeZone: "UTC"
+    }).formatRangeToParts(43201e3 * 4, 43201e3 * 5);
+    cb(range[0].type === "hour" && range[0].source === "startRange" && range[0].value === "0" && range[1].type === "literal" && range[1].source === "startRange" && range[1].value === " " && range[2].type === "dayPeriod" && range[2].source === "startRange" && range[2].value === "AM" && range[3].type === "literal" && range[3].source === "shared" && range[4].type === "hour" && range[4].source === "endRange" && range[4].value === "0" && range[5].type === "literal" && range[5].source === "endRange" && range[5].value === " " && range[6].type === "dayPeriod" && range[6].source === "endRange" && range[6].value === "PM");
   })(callback);
 })();
