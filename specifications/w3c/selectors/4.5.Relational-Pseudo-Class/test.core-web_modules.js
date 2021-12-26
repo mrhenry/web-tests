c53d623ee8855c26aceb7c10aec52b820e88b153
romainmenke <romainmenke@gmail.com>
2021-12-26 16:15:01 +0100

    The Relational Pseudo-class

---
+++ specifications/w3c/selectors/4.5.Relational-Pseudo-Class/test.core-web_modules.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+(function (cb) {
+  var foo = document.querySelector('div:has(> #the-fixture--positioned, :does-not-exist)');
+  cb(foo.matches(':has(> #the-fixture--positioned)'));
+})(callback);
+/******/ })()
+;
---
+++ specifications/w3c/selectors/4.5.Relational-Pseudo-Class/test.pure.js
@@ -0,0 +1,4 @@
+(function (cb) {
+	var foo = document.querySelector('div:has(> #the-fixture--positioned, :does-not-exist)');
+	cb(foo.matches(':has(> #the-fixture--positioned)'));
+})(callback);
