770444c6fe2c36e867d1a8f88aa670a224902a64
romainmenke <romainmenke@gmail.com>
2022-08-20 11:48:17 +0200

    edge serializes colors differently

--- specifications/w3c/svg2/6.1.Styling_Invalid-Property-Values/test.pure.js
+++ specifications/w3c/svg2/6.1.Styling_Invalid-Property-Values/test.pure.js
@@ -1,12 +1,15 @@
 (function (cb) {
+	// Notes:
+	// - Edge serializes colors differently
+
 	var style1 = window.getComputedStyle(document.getElementById('a-rect-1'));
-	if (style1.fill !== 'rgb(255, 0, 0)') {
+	if (style1.fill !== 'rgb(255, 0, 0)' && style1.fill !== 'red') {
 		cb(false);
 		return;
 	}
 
 	var style2 = window.getComputedStyle(document.getElementById('a-rect-2'));
-	if (style2.fill !== 'rgb(255, 0, 0)') {
+	if (style2.fill !== 'rgb(255, 0, 0)' && style2.fill !== 'red') {
 		cb(false);
 		return;
 	}
