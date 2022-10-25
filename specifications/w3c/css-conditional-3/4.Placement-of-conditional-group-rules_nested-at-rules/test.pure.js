02c26ff1e1ab02d4b7f0cd905079193cdd3325a1
Romain Menke <romainmenke@gmail.com>
2022-10-25 18:07:06 +0200

    fixes

--- specifications/w3c/css-conditional-3/4.Placement-of-conditional-group-rules_nested-at-rules/test.pure.js
+++ specifications/w3c/css-conditional-3/4.Placement-of-conditional-group-rules_nested-at-rules/test.pure.js
@@ -1,12 +1,21 @@
 (function (cb) {
-	setTimeout(function() {
+	if ('requestAnimationFrame' in window) {
+		requestAnimationFrame(function () {
+			var style = window.getComputedStyle(document.getElementById('hello'));
+			if (style.color !== 'rgb(255, 0, 0)') {
+				cb(false);
+				return;
+			}
+
+			cb(true);
+		});
+	} else {
 		var style = window.getComputedStyle(document.getElementById('hello'));
-		console.log(style.color);
 		if (style.color !== 'rgb(255, 0, 0)') {
 			cb(false);
 			return;
 		}
 
 		cb(true);
-	}, 200);
+	}
 })(callback);
