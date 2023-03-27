2519b79f359f3dfe372e5cfb61f881e94a7b26ea
Romain Menke <romainmenke@gmail.com>
2023-03-27 19:00:11 +0200

    Revert "extra test"
    This reverts commit 60657de496b370c2db419a7f8d572bd7234ea8fa.

--- specifications/w3c/css-images-4/3.1.ColorInterpolationMethod/test.pure.js
+++
@@ -1,9 +0,0 @@
-(function (cb) {
-	var style = window.getComputedStyle(document.getElementById('hello'));
-	if (style.background === 'rgb(255, 0, 0)') {
-		cb(false);
-		return;
-	}
-
-	cb(true);
-})(callback);
