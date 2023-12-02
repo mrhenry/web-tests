99e2c1441cddf11b3eeffc1b1f799b7950266b15
Romain Menke <romainmenke@gmail.com>
2023-12-02 15:03:25 +0100

    fix

--- specifications/w3c/css-color-4/9.3.Mixed-Components/test.pure.js
+++ specifications/w3c/css-color-4/9.3.Mixed-Components/test.pure.js
@@ -1,13 +1,13 @@
 (function (cb) {
 	var style;
 	style = window.getComputedStyle(document.querySelector('.foo'));
-	if (style.color !== 'lch(46 40 134.38)') {
+	if (style.color !== 'lch(46 10 134.38)') {
 		cb(false);
 		return;
 	}
 
 	style = window.getComputedStyle(document.querySelector('.bar'));
-	if (style.color !== 'lab(46 -40 50)') {
+	if (style.color !== 'lab(46 -10 25)') {
 		cb(false);
 		return;
 	}
